a581dde9695927768bd1ccd34370ad2727e49922
Mehourka <k.mehour@gmail.com>
2023-08-17 18:40:28 +0000

    fix: fix type query using LEFT JOIN

--- element.sh
+++ element.sh
@@ -8,7 +8,6 @@ if [[ ! $1 ]]; then
   exit
 fi
 
-
 ELEMENT=$1
 # Check if input is Atomic Number
 if [[ $ELEMENT =~ ^[0-9]+$ ]]; then
@@ -32,11 +31,10 @@ then
 else
   NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ELEMENT_ID")
   SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ELEMENT_ID")
-  TYPE=$($PSQL "SELECT type FROM properties WHERE atomic_number=$ELEMENT_ID")
+  TYPE=$($PSQL "SELECT type FROM properties LEFT JOIN types USING(type_id) WHERE atomic_number=$ELEMENT_ID")
   MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ELEMENT_ID")
   MELT_TEMP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ELEMENT_ID")
   BOIL_TEMP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ELEMENT_ID")
 
   echo "The element with atomic number $ELEMENT_ID is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT_TEMP celsius and a boiling point of $BOIL_TEMP celsius."
 fi
-
