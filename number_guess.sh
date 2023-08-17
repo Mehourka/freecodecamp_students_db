ae5246323a73f4a9387a5200f63bc6242e347b76
Mehourka <k.mehour@gmail.com>
2023-08-17 20:18:58 +0000

    feat: add boilerplate condition and print messages

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-
-
 # Programm that makes you guess a random number
+
+# Fetch user name
 echo "Enter your username:"
-read USERNAME
+read USER_NAME
+
+# If username exists
+if false; then
+  # gather user stats
+  # print greeting and stats
+  echo "Should not print"
+# else
+else
+  # greet new user
+  echo "Welcome, $USER_NAME! It looks like this is your first time here."
+  # register new user
+fi
+
+echo "Guess the secret number between 1 and 1000:"
